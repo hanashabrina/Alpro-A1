f7e7309bec64830ed82ff7abc87845d53e62c19c
hanashabrina <100180248+hanashabrina@users.noreply.github.com>
2022-03-01 19:25:00 +0700

    Add files via upload

---
+++ Praktikum2/luasbjsk.c
@@ -0,0 +1,21 @@
+/*Nama File	: luasbjsk.c*/
+/*Deskripsi	: Menghitung luas dari bujur sangkar*/
+/*Pembuat	: Hana Shabrina-24060121130077*/
+/*Tgl pembuatan	: Selasa, 01-03-2022 19.20*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*Kamus*/
+    int s;
+    int l;
+    s = 20;
+
+/*Algoritma*/
+    l = s*s;
+    printf("Sisi = %d\n", s);
+    printf("Luas Bujursangkar = %d\n", l);
+
+    return 0;
+}
