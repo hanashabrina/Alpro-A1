d4df7c1326a4500c677c736e84282e5cedc958bd
hanashabrina <100180248+hanashabrina@users.noreply.github.com>
2022-05-06 20:43:39 +0700

    Update BubbleSort.c

--- Praktikum9/BubbleSort.c
+++ Praktikum9/BubbleSort.c
@@ -1,29 +1,49 @@
 /*Nama File	: BubbleSort.c*/
-/*Deskripsi	: Mengurutkan data dari yang terkecil ke terbesar dengan bubble sorting*/
+/*Deskripsi	: Mengurutkan data menggunakan bubble sorting*/
 /*Pembuat	: Hana Shabrina-24060121130077*/
-/*Tgl pembuatan : Jumat, 06-05-2022 11.00 WIB*/
+/*Tgl pembuatan : Jumat, 06-05-2022 20.00 WIB*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Sub Program*/
-void BubbleSort(int arr[], int n)
+/*Sub Program untuk mengurutkan dari data yang terkecil ke terbesar*/
+void BubbleSortAsc(int arr[], int n)
 {
     /*Kamus Lokal*/
-    int i;
     int j;
-    int tmp;
+    int i;
+    int temp;
 
     /*Algoritma*/
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = tmp;
-            }
-        }
-    }
+	for(int i = 0; i < n-1; i++){
+		for(int j = 0; j < n-1; j++){
+			if(arr[j+1] < arr[j]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+
+/*Sub Program untuk mengurutkan dari data yang terbesar ke terkecil*/
+void BubbleSortDesc(int arr[], int n)
+{
+    /*Kamus Lokal*/
+    int j;
+    int i;
+    int temp;
+
+    /*Algoritma*/
+	for(int i = 0; i < n-1; i++){
+		for(int j = 0; j < n-1; j++){
+			if(arr[j+1] > arr[j]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
 }
 
 /*Program Utama*/
@@ -40,18 +60,26 @@ int main()
     printf("Masukkan banyaknya jumlah data : ");
     scanf("%d", &n);
     printf("Masukkan data sebanyak %d\n", n);
-    for(i = 0; i < n; i++){
-        printf("Data ke-%d = ", i+1);
-        scanf("%d", &arr[i]);
-    }
-    printf("\n");
+	for(int i = 0; i < n; i++){
+		printf("Data ke-%d = ", i+1);
+		scanf("%d", &arr[i]);
+	}
+	printf("\n");
 
     /*Memanggil Sub Program*/
-    BubbleSort(arr, n);
-    printf("Hasil pengurutan sebagai berikut :\n");
-    for(i = 0; i < n; i++){
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 0;
+	BubbleSortAsc(arr, n);
+	printf("Setelah Sorting Ascending : \n");
+	for(int i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	/*Memanggil Sub Program*/
+	BubbleSortDesc(arr, n);
+	printf("\nSetelah Sorting Descending : \n");
+	for(int i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
 }
